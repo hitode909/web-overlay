6be39a00b217eabe4af1e5fc65b590c37d0db0a9
hitode909 <hitode909@gmail.com>
2013-01-27 02:21:13 +0900

    useful example

---
+++ examples/highlight-images-without-alt.js
@@ -0,0 +1,10 @@
+{
+    "url": "http://nytimes.com",
+    "overlays": [
+        {
+            "selector": "img:visible:not([alt])",
+            "label": "missing alt",
+            "type": "all"
+        }
+    ]
+}
