212c931c1f86fc14bfe87ab866d39ad78cc3865a
hitode909 <hitode909@gmail.com>
2013-02-09 14:41:46 +0900

    add an example

---
+++ examples/apple-header-nav.js
@@ -0,0 +1,16 @@
+{
+    "url": "http://apple.com",
+    "overlays": [
+        {
+            "selector": "#globalnav li",
+            "label": "li",
+            "type": "all"
+        },
+        {
+            "selector": "#globalnav",
+            "label": "#globalnav",
+            "type": "all"
+        },
+
+    ]
+}
